a6cbe8df4d3a9a79e814921a4bfa318df44f2dbb
littlephillips <phillipkinuthia72@gmail.com>
2023-08-18 02:52:27 +0300

    added sound notification

--- src/pages/Timer/Timer.js
+++ src/pages/Timer/Timer.js
@@ -1,14 +1,20 @@
+// Timer.js
 import React, { useState, useEffect } from 'react';
+import alarmSound from '../../assests/alarmSound.wav'; 
 
 // style
-import '../../styles/Timer.css'
+import '../../styles/Timer.css';
+
+
 
 const Timer = () => {
-  const [workTime, setWorkTime] = useState(25);
-  const [breakTime, setBreakTime] = useState(5);
+  const [workTime, setWorkTime] = useState(1);
+  const [breakTime, setBreakTime] = useState(1);
   const [isWorking, setIsWorking] = useState(true);
   const [isActive, setIsActive] = useState(false);
   const [timeLeft, setTimeLeft] = useState(workTime * 60);
+  const [tasks, setTasks] = useState([]);
+  const audioRef = React.createRef();
 
   useEffect(() => {
     let interval;
@@ -18,21 +24,21 @@ const Timer = () => {
         setTimeLeft(prevTime => prevTime - 1);
       }, 1000);
     } else if (isActive && timeLeft === 0) {
+      audioRef.current.play(); // Play notification sound
       setIsWorking(prevIsWorking => !prevIsWorking);
       setTimeLeft(isWorking ? breakTime * 60 : workTime * 60);
+      setTasks(prevTasks => [...prevTasks, isWorking ? 'Work' : 'Break']);
     }
 
     return () => clearInterval(interval);
-  }, [isActive, timeLeft, workTime, breakTime, isWorking]);
+  }, [isActive, timeLeft, workTime, breakTime, isWorking, audioRef]);
 
-  // Format time function 
   const formatTime = seconds => {
     const minutes = Math.floor(seconds / 60);
     const formattedMinutes = String(minutes).padStart(2, '0');
     const formattedSeconds = String(seconds % 60).padStart(2, '0');
     return `${formattedMinutes}:${formattedSeconds}`;
   };
-  
 
   return (
     <div className="timer">
@@ -46,9 +52,17 @@ const Timer = () => {
           Reset
         </button>
       </div>
+      <div className="task-list">
+        <h2>Task List</h2>
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>{task}</li>
+          ))}
+        </ul>
+      </div>
+      <audio ref={audioRef} src={alarmSound}></audio>
     </div>
   );
-  
 };
 
 export default Timer;

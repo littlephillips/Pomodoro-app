6e893a270089f670c240234afe6a245a80cf0b8c
littlephillips <phillipkinuthia72@gmail.com>
2023-08-18 02:44:07 +0300

    added toggle for light and dark theme

--- src/components/AppComponent/App.js
+++ src/components/AppComponent/App.js
@@ -1,15 +1,23 @@
-import React from 'react'
-import Timer from '../../pages/Timer/Timer'
-
+import React, { useState } from 'react';
+import Timer from '../../pages/Timer/Timer';
 // style
-import '../../styles/App.css'
+import '../../styles/App.css';
 
 function App() {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    setTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
   return (
-    <div className='app'>
-    < Timer />
+    <div className={`app ${theme}`}>
+      <button className="theme-toggle" onClick={toggleTheme}>
+        Toggle Theme
+      </button>
+      <Timer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
